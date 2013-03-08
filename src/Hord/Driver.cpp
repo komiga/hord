b965e50105eb0d2484ceb7a1f2c70689f79dfe0a
Coranna Howard <me@komiga.com>
2013-03-08 00:21:36 -0500

    Driver: switched to error reporting macros.

--- src/Hord/Driver.cpp
+++ src/Hord/Driver.cpp
@@ -8,17 +8,31 @@ namespace Hord {
 
 // class Driver implementation
 
+#define HORD_SCOPE_CLASS_IDENT__ Driver
+
 Hive& Driver::placehold_hive(String root) {
+#define HORD_SCOPE_FUNC_IDENT__ placehold_hive
 	if (root.empty()) {
-		throw Error{ErrorCode::driver_hive_root_empty, HORD_STR_LIT("Driver: Cannot placehold hive with empty root path")};
+		HORD_THROW_ERROR_SCOPED(
+			ErrorCode::driver_hive_root_empty,
+			"cannot placehold hive with empty root path"
+		);
 	} else if (
 		m_hives.cend()!=std::find_if(m_hives.cbegin(), m_hives.cend(),
-			[&root](Hive const& hive) -> bool { return 0==root.compare(hive.get_root()); })
+			[&root](Hive const& hive) -> bool {
+				return 0==root.compare(hive.get_root());
+			})
 	) {
-		throw Error{ErrorCode::driver_hive_root_shared, HORD_STR_LIT("Driver: Cannot placehold hive with non-unique root path")};
+		HORD_THROW_ERROR_SCOPED_FQN(
+			ErrorCode::driver_hive_root_shared,
+			"cannot placehold hive with non-unique root path"
+		);
 	}
 	m_hives.emplace_back(std::move(root));
 	return m_hives.back();
+#undef HORD_SCOPE_FUNC_IDENT__
 }
+
+#undef HORD_SCOPE_CLASS_IDENT__
 
 } // namespace Hord
