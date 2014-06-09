9ec0668064cd616320afd4707a1ccd1773c99a70
Coranna Howard <me@komiga.com>
2014-06-09 02:56:50 -0400

    Data: added FieldMask enum and serialization functions.

--- include/Hord/Data/Defs.hpp
+++ include/Hord/Data/Defs.hpp
@@ -30,7 +30,8 @@ enum class MetaFieldType : std::uint8_t;
 	@note The bitflag layout is only for Rule::type_info to
 	restrict use; Fields must use a single value.
 
-	@sa Data::Field
+	@sa Data::Field,
+		Data::FieldMask
 */
 enum class FieldType : std::uint8_t {
 	/** Textual field. */
@@ -41,6 +42,36 @@ enum class FieldType : std::uint8_t {
 
 	/** Boolean field. */
 	Boolean	= 1 << 2
+};
+
+/**
+	Field type masks.
+
+	@remarks Distinct type for restriction purposes
+	(e.g., Node::Column).
+
+	@sa Data::Field,
+		Data::FieldType
+*/
+enum class FieldMask : std::uint8_t {
+	/** Textual field. */
+	Text	= enum_cast(Data::FieldType::Text),
+
+	/** Numeric field. */
+	Number	= enum_cast(Data::FieldType::Number),
+
+	/** Boolean field. */
+	Boolean	= enum_cast(Data::FieldType::Boolean),
+
+	/** No types. */
+	none = 0,
+
+	/** All types. */
+	any
+		= FieldMask::Text
+		| FieldMask::Number
+		| FieldMask::Boolean
+	,
 };
 
 /**
@@ -71,6 +102,32 @@ enum class MetaFieldType : std::uint8_t {
 	LAST
 /** @endcond */
 };
+
+/**
+	Serialize FieldType.
+*/
+template<class Ser>
+inline ser_result_type
+serialize(
+	ser_tag_serialize,
+	Ser& ser,
+	Data::FieldType& value
+) {
+	ser(Cacophony::make_enum_cfg<std::uint8_t>(const_safe<Ser>(value)));
+}
+
+/**
+	Serialize FieldMask.
+*/
+template<class Ser>
+inline ser_result_type
+serialize(
+	ser_tag_serialize,
+	Ser& ser,
+	Data::FieldMask& value
+) {
+	ser(Cacophony::make_enum_cfg<std::uint8_t>(const_safe<Ser>(value)));
+}
 
 /** @} */ // end of doc-group metadata
 /** @} */ // end of doc-group data
