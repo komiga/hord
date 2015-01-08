fcf1fba2747da54a6304a5b12b1cb3fe02617401
Coranna Howard <me@komiga.com>
2015-01-07 22:25:25 -0500

    Data/Ops: added ValueRef printer.

---
+++ include/Hord/Data/Ops.hpp
@@ -0,0 +1,76 @@
+/**
+@copyright MIT license; see @ref index or the accompanying LICENSE file.
+
+@file
+@brief Data operations.
+@ingroup data
+*/
+
+#pragma once
+
+#include <Hord/config.hpp>
+#include <Hord/Data/Defs.hpp>
+
+#include <iosfwd>
+
+namespace Hord {
+namespace Data {
+
+// Forward declarations
+struct ValueRefPrinter;
+
+/**
+	@addtogroup data
+	@{
+*/
+
+/**
+	Value printer.
+*/
+struct ValueRefPrinter {
+/** @name Properties */ /// @{
+	/**
+		Value.
+
+		@note The referred object must exist as long as the printer.
+	*/
+	Data::ValueRef const& value;
+/// @}
+
+private:
+	ValueRefPrinter() = delete;
+
+public:
+/** @name Special member functions */ /// @{
+	/** Destructor. */
+	~ValueRefPrinter() noexcept = default;
+
+	/** Copy constructor. */
+	ValueRefPrinter(ValueRefPrinter const&) noexcept = default;
+	/** Move constructor. */
+	ValueRefPrinter(ValueRefPrinter&&) noexcept = default;
+
+	/**
+		Constructor with value.
+	*/
+	ValueRefPrinter(
+		Data::ValueRef const& value
+	) noexcept
+		: value(value)
+	{}
+/// @}
+};
+
+/**
+	Write value to stream as string.
+*/
+std::ostream&
+operator<<(
+	std::ostream& stream,
+	Data::ValueRefPrinter const& printer
+);
+
+/** @} */ // end of doc-group data
+
+} // namespace Data
+} // namespace Hord
---
+++ src/Hord/Data/Ops.cpp
@@ -0,0 +1,71 @@
+/**
+@copyright MIT license; see @ref index or the accompanying LICENSE file.
+*/
+
+#include <Hord/utility.hpp>
+#include <Hord/Data/Defs.hpp>
+#include <Hord/Data/Ops.hpp>
+#include <Hord/Object/Ops.hpp>
+
+#include <ostream>
+
+#include <Hord/detail/gr_ceformat.hpp>
+
+namespace Hord {
+namespace Data {
+
+// class ValueRef implementation
+
+#define HORD_SCOPE_CLASS ValueRef
+
+namespace {
+static constexpr ceformat::Format const
+	s_fmt_integer_signed{"%d"},
+	s_fmt_integer_unsigned{"%u"},
+	s_fmt_decimal{"%f"}
+;
+} // anonymous namespace
+
+std::ostream&
+operator<<(
+	std::ostream& stream,
+	Data::ValueRefPrinter const& printer
+) {
+	Data::ValueRef const& value = printer.value;
+	switch (value.type.type()) {
+	case Data::ValueType::null:
+		stream << "(null)";
+		break;
+
+	case Data::ValueType::dynamic:
+		// NB: Should never happen
+		stream << "(dynamic)";
+		break;
+
+	case Data::ValueType::integer:
+		if (enum_bitand(value.type.flags(), Data::ValueFlag::integer_signed)) {
+			ceformat::write<s_fmt_integer_signed>(stream, value.integer_signed());
+		} else {
+			ceformat::write<s_fmt_integer_unsigned>(stream, value.integer_unsigned());
+		}
+		break;
+
+	case Data::ValueType::decimal:
+		ceformat::write<s_fmt_decimal>(stream, value.decimal());
+		break;
+
+	case Data::ValueType::object_id:
+		stream << value.data.object_id;
+		break;
+
+	case Data::ValueType::string:
+		stream.write(value.data.string, value.size);
+		break;
+	}
+	return stream;
+}
+
+#undef HORD_SCOPE_CLASS
+
+} // namespace Data
+} // namespace Hord
