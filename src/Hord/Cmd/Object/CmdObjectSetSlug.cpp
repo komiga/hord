997191d5e4d47b7076218e374634efe277764435
Coranna Howard <me@komiga.com>
2015-01-14 11:48:33 -0500

    Cmd::Object::SetSlug: check for slug collision in parent.

--- src/Hord/Cmd/Object/CmdObjectSetSlug.cpp
+++ src/Hord/Cmd/Object/CmdObjectSetSlug.cpp
@@ -26,17 +26,32 @@ HORD_SCOPE_CLASS::operator()(
 	Hord::Object::Unit& object,
 	String new_slug
 ) noexcept try {
+	auto& datastore = get_datastore();
 	m_id = object.get_id();
 
-	// TODO: Check for collision in parent children
 	if (new_slug.empty()) {
 		return commit_error("slug cannot be empty");
 	} else if (Hord::Object::SLUG_MAX_SIZE < new_slug.size()) {
 		return commit_error("slug must be <= 255 code units");
 	} else if (object.get_slug() == new_slug) {
 		return commit_with(Cmd::Result::success_no_action);
+	} else {
+		// Check for collision in parent children
+		auto const slug_hash = hash_string(new_slug);
+		auto const* const parent = datastore.find_ptr(object.get_parent());
+		auto const& parent_children
+			= parent
+			? parent->get_children()
+			: get_datastore().get_root_objects()
+		;
+		for (auto const child_id : parent_children) {
+			auto const* const child = datastore.find_ptr(child_id);
+			if (child && child->get_slug_hash() == slug_hash) {
+				return commit_error("slug non-unique in parent's child space");
+			}
+		}
 	}
-	object.set_slug(new_slug);
+	object.set_slug(std::move(new_slug));
 	object.get_prop_states().assign(
 		IO::PropType::identity,
 		IO::PropState::modified
